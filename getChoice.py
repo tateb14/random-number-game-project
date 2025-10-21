5f7b4cd3dcf592973da5ada966970d135637af56
tate.b <121896551+tateb14@users.noreply.github.com>
2025-10-21 12:10:31 -0500

    Create get_choice.py
    Created inputs for the choice
    Validated the inputs to be between 1-3
    Returns the choice number.

---
+++ getChoice.py
@@ -0,0 +1,16 @@
+# getChoice.py
+
+def get_choice():
+    # get_choice accepts no args
+    # which will prompt the user to input a number
+    # this will validate the inputted number
+    # return the correct output, starting a new game
+    # choosing a range
+    # or exitting the program.
+    print("Welcome to the random number guessing game! Please select one of the options below!\n\n1: Start a new game\n2: Define a new range\n3: Exit")
+    choice = int(input(">: "))
+    
+    while choice < 1 or choice > 3:
+        choice = int(input("Please input a valid number between 1-3: "))
+    
+    return choice
