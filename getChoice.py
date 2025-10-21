faaf11d6c028d50d6fc9356ad886d898c28e61bc
tate.b <121896551+tateb14@users.noreply.github.com>
2025-10-21 12:45:02 -0500

    refactor: commented file
    Added comments for easier review

--- getChoice.py
+++ getChoice.py
@@ -7,10 +7,10 @@ def get_choice():
     # return the correct output, starting a new game
     # choosing a range
     # or exitting the program.
-    print("Welcome to the random number guessing game! Please select one of the options below!\n\n1: Start a new game\n2: Define a new range\n3: Exit")
-    choice = int(input(">: "))
+    print("Welcome to the random number guessing game! Please select one of the options below!\n\n1: Start a new game\n2: Define a new range\n3: Exit") # Displays the legend
+    choice = int(input(">: ")) # Prompts the user for the choice
     
-    while choice < 1 or choice > 3:
+    while choice < 1 or choice > 3: # Validates the choice and prompts the user to reinput the number if required
         choice = int(input("Please input a valid number between 1-3: "))
     
-    return choice
+    return choice # Returns choice
