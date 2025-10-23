08fc0192bc2b0b2b3191fee88c4b7c57bb87a200
tate.b <121896551+tateb14@users.noreply.github.com>
2025-10-23 11:50:15 -0500

    added all functions to main

---
+++ main.py
@@ -0,0 +1,23 @@
+def main():
+    pass
+
+def menu():
+    pass
+
+def start_game():
+    pass
+
+def choose_range():
+    pass
+
+def exit_game():
+    pass
+
+def get_players():
+    pass
+
+def num_gen():
+    pass
+
+def guess_num():
+    pass
